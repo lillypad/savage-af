816bf5eefc1350a6525426e4df249ee456331849
lillypad <lchalupowski@gosecure.ca>
2017-12-27 22:15:56 -0400

    update

--- src/main.c
+++ src/main.c
@@ -9,32 +9,47 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <openssl/ssl.h>
+#include <regex.h>
 
 #define MAX_DOMAIN_SIZE 253
 
-int validate_ip(char *ip){
-  struct sockaddr_in sa;
-  int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
-  return result;
-}
-
 int validate_port(int port){
-  // Validate Port
   if (port < 0 || port > 65535){
-    return -1;
-  } else{
     return 1;
   }
+  return 0;
 }
 
 int validate_domain(char *domain){
-  // Validate Domain
   if (sizeof(domain) > MAX_DOMAIN_SIZE){
     fprintf(stderr, "Invalid domain name!\n");
-    return -1;
-  } else{
     return 1;
   }
+  regex_t regDomain;
+  if (regcomp(&regDomain, "^[a-zA-Z0-9][a-zA-Z0-9.-]*$", 0) != 0){
+    fprintf(stderr, "ERROR: failed to compile regDomain\n");
+    return 1;
+  }
+  if (regexec(&regDomain, domain, 0, NULL, 0) != 0){
+    regfree(&regDomain);
+    return 1;
+  }
+  regfree(&regDomain);
+  return 0;
+}
+
+int validate_ip(char *ip){
+  regex_t regIPV4;
+  if (regcomp(&regIPV4, "^[:digit:]{1,3}[.][:digit:]{1,3}[.][:digit:]{1,3}[.][:digit:]{1,3}", 0) != 0){
+    fprintf(stderr, "ERROR: failed to compile regIPV4\n");
+    return 1;
+  }
+  if (regexec(&regIPV4, ip, 0, NULL, 0) != 0){
+    regfree(&regIPV4);
+    return 1;
+  }
+  regfree(&regIPV4);
+  return 0;
 }
 
 char *i2cPort(int port){
@@ -46,47 +61,44 @@ char *i2cPort(int port){
   }
 }
 
-int http_send(char *domain, int port, char *data){
+int http_send_domain(char * domain, int port, char *data){
+  if (validate_domain(domain) != 0){
+    return 1;
+  }
   int sockfd, connectfd;
   struct addrinfo hints, *servinfo, *p;
-  if (validate_port(port) < 0){ return -1; }
-  if (validate_domain(domain) < 0){ return -2; }
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  int result = getaddrinfo(domain, i2cPort(port), &hints, &servinfo);
-  if (result != 0){
-    fprintf(stderr, "Error obtaining address info.");
-    return -3;
-  } else{
-    printf("Successfully collected address info!\n");
+  if (getaddrinfo(domain, i2cPort(port), &hints, &servinfo) != 0){
+    freeaddrinfo(servinfo);
+    return 1;
   }
   for(p = servinfo; p != NULL; p = p->ai_next){
     sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
     if (sockfd < 0){
-      fprintf(stderr, "Error opening socket\n");
       continue;
-    } else{
-      printf("Opened socket successfully!\n");
     }
     connectfd = connect(sockfd, p->ai_addr, p->ai_addrlen);
     if (connectfd < 0){
-      fprintf(stderr, "Failed to connect to socket\n");
       close(sockfd);
       continue;
+    }
+    if (send(sockfd, data, strlen(data), 0) < 0){
+      continue;
     } else{
-      printf("Successfully connected to socket!\n");
-      result = send(sockfd, data, strlen(data), 0);
-      if (result < 0){
-        fprintf(stderr, "Failed to send data!\n");
-        return -4;
-      } else{
-        printf("Successfully sent data!\n");
-        close(sockfd);
-      }
-      break;
+      close(sockfd);
+      freeaddrinfo(servinfo);
+      return 0;
     }
   }
   freeaddrinfo(servinfo);
+  return 1;
+}
+
+
+int main(){
+  int result = validate_ip("8.8.8.8");
+  printf("%d\n", result);
   return 0;
 }
